d8c66b2b161f19e66a7b4a511d313f4d3abb3be8
john keru <keruk355@gmail.com>
2024-10-25 00:47:57 +0800

    template v1

--- SERVER/static/setRolesAndSuperAdmin.js
+++ SERVER/static/setRolesAndSuperAdmin.js
@@ -7,31 +7,37 @@ exports.setRolesAndAssign = async (_req, res) => {
             name: process.env.SUPER_ADMIN, // 'SUPER ADMIN'
             permission: 'Has full access to all systems.' // SUPER ADMIN has full permission
         },
-    ]
+    ];
+
     try {
         // Delete all existing roles
         await role.deleteMany({});
 
-        roles.map(async ({ name, permission }) => {
-            const newRole = new role({ name, permission });
-            await newRole.save()
-        })
+        // Create roles and save them using Promise.all
+        await Promise.all(
+            roles.map(({ name, permission }) => {
+                const newRole = new role({ name, permission });
+                return newRole.save();
+            })
+        );
 
-        const adminRole = await role.findOne({ name: process.env.SUPER_ADMIN })
-        const superAdmins = [process.env.SUPER_ADMIN_1,] // list all super admins here
+        const adminRole = await role.findOne({ name: process.env.SUPER_ADMIN });
+        const superAdmins = [process.env.SUPER_ADMIN_1]; // list all super admins here
 
         // Delete all existing roles assigned to super admins
-        await assignedRole.deleteMany({})
+        await assignedRole.deleteMany({});
 
-        superAdmins.forEach(async (adminId) => {
-            const assignRole = new assignedRole({ assignedBy: adminId, user: adminId, roles: [adminRole._id] })
-            await assignRole.save()
-        })
+        // Assign roles to super admins using Promise.all
+        await Promise.all(
+            superAdmins.map(adminId => {
+                const assignRole = new assignedRole({ assignedBy: adminId, user: adminId, roles: [adminRole._id] });
+                return assignRole.save();
+            })
+        );
 
         res.json({ message: 'Roles set and Super Admins set successfully' });
-
     } catch (e) {
         console.error(e);
         res.status(500).json({ error: 'Server error' });
     }
-}
+};
