e67b6413647bef2c8e45ab2e15e557cee091f222
john keru <80145233+johnkeru@users.noreply.github.com>
2024-11-29 13:31:03 +0800

    Update UserContext.jsx

--- CLIENT/src/context/UserContext.jsx
+++ CLIENT/src/context/UserContext.jsx
@@ -1,7 +1,6 @@
 
 import { createContext, useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import api from "../config/api";
+import { authApi } from "../config/api";
 import LoadingPage from "../global/components/Loading";
 
 const UserContext = createContext(
@@ -17,33 +16,32 @@ const UserContext = createContext(
 const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
-    const nav = useNavigate()
     // only use in login
     const handleSetUserInitialization = async () => {
-        const res = await api.get('/get-user')
+        const res = await authApi.get('/get-user')
         setCurrentUser(res.data.user)
     }
     const handleLogout = async () => {
-        await api.get('/logout')
+        await authApi.get('/logout')
         setCurrentUser(null)
-        nav('/login')
+        location.href='http://localhost:3000/login'
     }
 
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const res = await api.get('/get-user');
+                const res = await authApi.get('/get-user');
                 if (res.data.user) setCurrentUser(res.data.user);
-                else nav('/login');
+                else location.href='http://localhost:3000/login';
             } catch (error) {
                 console.error("Error fetching user:", error);
-                nav('/login');
+                location.href='http://localhost:3000/login';
             } finally {
                 setIsLoading(false);
             }
         };
         fetchUser();
-    }, [nav]); // Dependency array ensures useEffect runs once when the component mounts
+    }, []); // Dependency array ensures useEffect runs once when the component mounts
 
 
     const value = {
