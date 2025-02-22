70aad6736a9646f02b839b37d2a759abb4b1527d
keru <keruk355@gmail.com>
2025-02-22 08:39:34 +0800

    done 90%

--- CLIENT/src/global/components/CustomAutoComplete.jsx
+++ CLIENT/src/global/components/CustomAutoComplete.jsx
@@ -1,44 +1,63 @@
-import { Controller } from "react-hook-form";
+import { useController } from "react-hook-form";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
 const CustomAutoComplete = ({
-  fieldName = "",
+  fieldName,
   control,
-  errors,
-  dataOption,
   onInputChange,
+  dataOption = [],
+  getOptionLabel = "",
+  onChange = undefined,
   sx = { bgcolor: "#ffffff", m: 0 },
   size = "small",
   variant = "outlined",
+  required = false,
+  freeSolo = false,
   ...rest
 }) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    name: fieldName,
+    control,
+    rules: required ? { required: "This field is required" } : undefined,
+  });
+
   return (
-    <Controller
-      name={fieldName}
-      control={control}
-      rules={{ required: "This field is required" }}
-      render={({ field }) => (
-        <Autocomplete
-          {...field}
-          options={dataOption || []}
-          getOptionLabel={(option) => option || ""}
-          value={field.value || null} // Ensure initial value is set to null or a valid object
-          onChange={(_, value) => field.onChange(value)}
-          onInputChange={onInputChange && onInputChange}
-          renderInput={(params) => (
-            <TextField
-              required
-              {...params}
-              error={!!errors[fieldName]}
-              helperText={errors[fieldName] ? errors[fieldName].message : ""}
-              variant={variant}
-              margin="normal"
-              size={size}
-              sx={sx}
-              {...rest}
-            />
-          )}
+    <Autocomplete
+      freeSolo={freeSolo}
+      {...field}
+      options={dataOption}
+      getOptionLabel={(option) =>
+        typeof option === "string" ? option : option[getOptionLabel] || ""
+      }
+      getOptionKey={(option) =>
+        typeof option === "string" ? option : option._id
+      }
+      value={field.value || null} // Ensure initial value is null or a valid object
+      onChange={(_, value) => {
+        // console.log(value);
+        field.onChange(value); // for use form change
+        onChange && onChange(value); // for dev use
+      }}
+      onInputChange={(_, value) => {
+        // console.log(value);
+        field.onChange(value); // for use form change
+        onInputChange && onInputChange(value); // for dev use
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          required={required}
+          error={!!error}
+          helperText={error ? error.message : ""}
+          variant={variant}
+          margin="normal"
+          size={size}
+          sx={sx}
+          {...rest}
         />
       )}
     />
